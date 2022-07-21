6bca53adc8689ad2a6d5bd6d3fd336e28fd7923f
bugbytes-io <bugbytes.io@gmail.com>
2022-07-21 12:25:00 +0100

    Final user-form code

--- core/forms.py
+++ core/forms.py
@@ -1,1 +1,14 @@
 from django import forms
+from core.models import Book
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book 
+        fields = ('name', 'author')
+
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if name.startswith("a"):
+            raise forms.ValidationError("Error: We don't want people who's names start with 'a'!")
+        return name
--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    name = models.CharField(max_length=128)
+    author = models.CharField(max_length=128)  # note: FK in real life
+
+    def __str__(self):
+        return self.name
